b7a60dcd306747ff12e67452921451021ceb9e72
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2016-03-07 20:56:46 +0530

    Moving the AppChecks struct to checks.go and removing marathon.go

--- checks.go
+++ checks.go
@@ -2,7 +2,18 @@ package main
 
 import (
 	"github.com/gambol99/go-marathon"
+	"time"
 )
+
+type AppCheck struct {
+	App       string
+	CheckName string
+	Result    CheckStatus
+	Message   string
+	Timestamp time.Time
+	Labels    map[string]string
+	Times     int
+}
 
 type Checker interface {
 	Name() string
